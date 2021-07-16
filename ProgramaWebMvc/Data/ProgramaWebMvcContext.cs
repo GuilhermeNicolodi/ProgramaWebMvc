aac95bec66c9a99bd9b3f015854aa0c2692a7958
GuilhermeNicolodi <gui.nicolodi@hotmail.com>
2021-07-16 01:26:29 -0300

    CRUD

---
+++ ProgramaWebMvc/Data/ProgramaWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProgramaWebMvc.Models;
+
+namespace ProgramaWebMvc.Data
+{
+    public class ProgramaWebMvcContext : DbContext
+    {
+        public ProgramaWebMvcContext (DbContextOptions<ProgramaWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProgramaWebMvc.Models.Department> Department { get; set; }
+    }
+}
